2951f36c72c9fc4bcbeb31e14eac4a38c11aebbd
SeptAlfauzan <alfauzansepta@gmail.com>
2023-04-09 06:04:04 +0700

    update! change board color scheme, add board logic, and reset method

--- app/src/main/java/com/example/tictactoe/MainActivity.kt
+++ app/src/main/java/com/example/tictactoe/MainActivity.kt
@@ -2,11 +2,31 @@ package com.example.tictactoe
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.tictactoe.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var _binding: ActivityMainBinding? = null
+    private val binding: ActivityMainBinding get() = _binding!!
+
+    companion object {
+        val TAG = MainActivity::class.java.simpleName
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         supportActionBar?.hide()
+
+        if(binding.board.isAllBoarAreasdUsed) Log.d(TAG, "onCreate: all board area is used")
+        binding.resetBtn.setOnClickListener {
+            resetGame()
+        }
+    }
+
+    private fun resetGame(){
+        binding.board.resetBoard()
     }
 }
--- app/src/main/java/com/example/tictactoe/ui/components/BoardView.kt
+++ app/src/main/java/com/example/tictactoe/ui/components/BoardView.kt
@@ -20,29 +20,44 @@ class BoardView(context: Context, attrs: AttributeSet): View(context, attrs) {
 
     private val paint = Paint()
     private val listArea = mutableListOf<Rectangle>()
+    private val _filledXArea = mutableListOf<Int>()
+    private val _filledOArea = mutableListOf<Int>()
     private val rowNumbers = 3
     private val columnNumbers = 3
     private var xDrawable: Drawable?
+    private var oDrawable: Drawable?
     private var xPos = -1f
     private var yPos = -1f
+
+    private var WIDTH: Int = 0
+    private var isResetTrigger = false
+
+    var isXturn = true
+    var isAllBoarAreasdUsed = false
+    val filledXArea: List<Int> get() = _filledXArea
+    val filledOArea: List<Int> get() = _filledOArea
+
+    private val bluePrimary = Color.parseColor("#7286D3")
+    private val bluePastel = Color.parseColor("#7286D3")
+    private val lightBluePastel = Color.parseColor("#E5E0FF")
+    private val accent = Color.parseColor("#FFF2F2")
 
     companion object {
         val TAG = BoardView::class.java.simpleName
     }
 
     init {
-        xDrawable = ContextCompat.getDrawable(context, R.drawable.baseline_close_24)
+        xDrawable = ContextCompat.getDrawable(context, R.drawable.baseline_close_62)
+        oDrawable = ContextCompat.getDrawable(context, R.drawable.outline_circle_62)
 
         paint.apply {
-            color = Color.RED
+            color = lightBluePastel
             style = Paint.Style.STROKE
             strokeWidth = 4f
         }
-        initAreas()
     }
 
-    private fun initAreas(){
-        val screenWidth = resources.displayMetrics.widthPixels
+    private fun initAreas(screenWidth: Float){
         val SQUARE_BASE_SIZE = screenWidth / 3
 
         for (row in 0..rowNumbers){
@@ -52,29 +67,48 @@ class BoardView(context: Context, attrs: AttributeSet): View(context, attrs) {
                 val right = left + SQUARE_BASE_SIZE
                 val bottom = top + SQUARE_BASE_SIZE
 
-                listArea.add(Rectangle(top.toFloat(), bottom.toFloat(), left.toFloat(), right.toFloat()))
+                listArea.add(Rectangle(top, bottom, left, right))
             }
         }
     }
 
-    private fun drawOnBoardPos(canvas: Canvas, position: Rectangle){
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+        val width = MeasureSpec.getSize(widthMeasureSpec)
+        val height = MeasureSpec.makeMeasureSpec(width, MeasureSpec.EXACTLY)
+        WIDTH = width
+
+        setMeasuredDimension(width, height)
+        initAreas(WIDTH.toFloat())
+    }
+
+    private fun drawOnBoardPos(canvas: Canvas, drawable: Drawable?, position: Rectangle){
 
         val xCenter = (position.right + position.left) / 2
         val yCenter = (position.bottom + position.top) / 2
 
-        xDrawable?.let {
+        drawable?.let {
             canvas.drawBitmap(it.toBitmap(), xCenter - it.intrinsicWidth/2, yCenter - it.intrinsicHeight/2, paint)
         }
+    }
+
+    fun resetBoard(){
+        isXturn = true
+        isResetTrigger = true
+        _filledXArea.clear()
+        _filledOArea.clear()
+        invalidate()
     }
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
 
-        val BASE_SIZE = width.toFloat()
+        val BASE_SIZE = WIDTH.toFloat()
         val SQUARE_BASE_SIZE = BASE_SIZE / 3
 
 //      draw outer rectangle
         canvas.drawRect(0f, 0f, BASE_SIZE, BASE_SIZE, paint)
+
 //      draw every mini squares in it
         for (row in 0..rowNumbers){
             for (column in 0..columnNumbers){
@@ -85,11 +119,28 @@ class BoardView(context: Context, attrs: AttributeSet): View(context, attrs) {
             }
         }
 
+        if(isResetTrigger){
+            isResetTrigger = false
+            return
+        }
+
         listArea.forEachIndexed { index, square ->
-            if(xPos > square.left && xPos < square.right && yPos > square.top && yPos < square.bottom){
-                Log.d(TAG, "onTouchEvent: $index")
-                drawOnBoardPos(canvas, square)
+            if(xPos > square.left && xPos < square.right && yPos > square.top && yPos < square.bottom){//fill the filled area with current area index pressed
+
+                if(_filledOArea.size + _filledXArea.size == listArea.size) isAllBoarAreasdUsed = true
+
+                if(!isAllBoarAreasdUsed && !_filledXArea.contains(index) && !_filledOArea.contains(index)){//add current index, if it's not already added
+                    if(isXturn) _filledXArea.add(index)
+                    else _filledOArea.add(index)
+                }
             }
+        }
+
+        _filledXArea.forEach{ xFilled ->
+            drawOnBoardPos(canvas, xDrawable, listArea[xFilled])
+        }
+        _filledOArea.forEach{ oFilled ->
+            drawOnBoardPos(canvas, oDrawable, listArea[oFilled])
         }
     }
 
@@ -97,8 +148,11 @@ class BoardView(context: Context, attrs: AttributeSet): View(context, attrs) {
         if(event.action == MotionEvent.ACTION_DOWN){
             xPos = event.x
             yPos = event.y
+
+            isXturn = !isXturn//change player turn
             invalidate()
         }
         return super.onTouchEvent(event)
     }
+
 }
